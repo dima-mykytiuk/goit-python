c5153e0e48fd806ac69462751dfe2c54935b9422
smilecool2012 <smilecool2012@gmail.com>
2021-10-17 12:29:18 +0300

    2 Build

--- DZ_6_sort/sort.py
+++ DZ_6_sort/sort.py
@@ -20,59 +20,56 @@ def normalize(name):
 
 def make_folders():
     for x in range(0, len(folder_list)):
-        if not os.path.exists(str(_dir) + folder_sep + folder_list[x]):
-            os.makedirs(str(_dir) + folder_sep + folder_list[x])
+        if not os.path.exists(f'{_dir}{folder_sep}{folder_list[x]}'):
+            os.makedirs(f'{_dir}{folder_sep}{folder_list[x]}')
 
 
 def move_files(path, item_name, file_type, folder_name, base_path_to):
-    if file_type in files_type.get(folder_name) and not os.path.exists(str(path) + folder_name + folder_sep + normalize(
-            item_name.removesuffix('.' + file_type)) + '.' + file_type):
-        FROM = str(path) + folder_sep + item_name
-        TO = str(base_path_to) + folder_sep + folder_name + folder_sep + normalize(
-            item_name.removesuffix('.' + file_type)) + '.' + file_type
-        shutil.move(FROM, TO)
+    check_exist_path = os.path.exists(f'{base_path_to}{folder_sep}{folder_name}{folder_sep}{item_name}')
+    if file_type in files_type.get(folder_name) and not check_exist_path:
+        path_from = f'{path}{folder_sep}{item_name}'
+        to_path = f'{base_path_to}{folder_sep}{folder_name}{folder_sep}{item_name}'
+        shutil.move(path_from, to_path)
 
 
-def find_sort_files(path):
+def find_files_and_sort(path):
     for item_name in os.listdir(path):
-        if item_name not in folder_list:
-            if os.path.isfile(path + folder_sep + item_name) and item_name != '.DS_Store':
-                file_type = item_name.split('.')[-1]
-                for folder_name in folder_list:
-                    if folder_name != 'archives':
-                        move_files(path, item_name, file_type, folder_name, main_path)
-                    elif file_type in files_type.get('archives') and not os.path.exists(
-                            str(path) + '/archives/' + normalize(item_name.removesuffix('.' + file_type))):
-                        shutil.unpack_archive(str(path) + folder_sep + item_name,
-                                              str(main_path) + '/archives/' + normalize(
-                                                  item_name.removesuffix('.' + file_type)))
-                        os.remove(str(path) + folder_sep + item_name)
-            elif os.path.isdir(path + folder_sep + item_name):
-                find_sort_files(path + folder_sep + item_name)
+        path_from = f'{path}{folder_sep}{item_name}'
+        if item_name not in folder_list and os.path.isfile(path_from) and item_name != '.DS_Store':
+            file_type = item_name.split('.')[-1]
+            rem_suffix = item_name.removesuffix(f'.{file_type}')
+            path_to_unpack = f'{main_path}{folder_sep}archives{folder_sep}{rem_suffix}'
+            for folder_name in folder_list:
+                if folder_name != 'archives':
+                    move_files(path, item_name, file_type, folder_name, main_path)
+                elif file_type in files_type.get('archives') and not os.path.exists(path_to_unpack):
+                    shutil.unpack_archive(path_from, path_to_unpack)
+                    os.remove(path_from)
+        elif item_name not in folder_list and os.path.isdir(path_from):
+            find_files_and_sort(path_from)
 
 
 def rename_files_and_folders(path):
     for folder_item in os.listdir(path):
-        if folder_item not in folder_list:
-            if os.path.isfile(path + folder_sep + folder_item) and folder_item != '.DS_Store':
-                file_type = folder_item.split('.')[-1]
-                if file_type not in files_type.keys():
-                    shutil.move(str(path) + folder_sep + folder_item,
-                                str(path) + folder_sep + normalize(
-                                    folder_item.removesuffix('.' + file_type)) + '.' + file_type)
-            elif os.path.isdir(path + folder_sep + folder_item):
-                shutil.move(str(path) + folder_sep + folder_item, str(path) + folder_sep + normalize(folder_item))
-                rename_files_and_folders(str(path) + folder_sep + normalize(folder_item))
+        path_to_folder = f'{path}{folder_sep}{folder_item}'
+        path_after_normalize = f'{path}{folder_sep}{normalize(folder_item)}'
+        file_type = folder_item.split('.')[-1]
+        rem_suffix = folder_item.removesuffix(f'.{file_type}')
+        normalize_name = f'{normalize(rem_suffix)}.{file_type}'
+        if os.path.isfile(path_to_folder) and folder_item != '.DS_Store':
+            shutil.move(path_to_folder, f'{path}{folder_sep}{normalize_name}')
+        elif os.path.isdir(path_to_folder):
+            shutil.move(path_to_folder, path_after_normalize)
+            rename_files_and_folders(path_after_normalize)
 
 
 def remove_empty_folders(path_abs):
     walk = list(os.walk(path_abs))
     for path, folders, folders_items in walk[::-1]:
         folder_name = path.split(folder_sep)[-1]
-        normal_path = folder_name not in folder_list and path == str(_dir) + folder_sep + folder_name
-        is_len_0 = len(os.listdir(path)) == 0 and normal_path
-        is_len_1 = len(os.listdir(path)) == 1 and '.DS_Store' in folders_items and normal_path
-        if is_len_0 or is_len_1:
+        len_0 = len(os.listdir(path)) == 0 and folder_name not in folder_list
+        len_1 = len(os.listdir(path)) == 1 and folder_name not in folder_list and '.DS_Store' in folders_items
+        if len_0 or len_1:
             shutil.rmtree(path)
 
 
@@ -92,6 +89,6 @@ if __name__ == '__main__':
         'documents': ['doc', 'docx', 'txt', 'pdf', 'xlsx', 'pptx'],
     }
     make_folders()
-    find_sort_files(str(_dir))
     rename_files_and_folders(str(_dir))
+    find_files_and_sort(str(_dir))
     remove_empty_folders(_dir)
