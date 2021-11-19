fb908250f7e76445d4d87827473c22ef8e2022af
smilecool2012 <smilecool2012@gmail.com>
2021-11-19 23:34:13 +0200

    11 Homework

--- DZ_11/HW_11.py
+++ DZ_11/HW_11.py
@@ -15,16 +15,12 @@ class AddressBook(UserDict):
 
     # Iterate over the data in the data dictionary and add this user data to the list for later output
     # I have used an iterator to return a certain number of values entered by the user. I have used list slice.
-    # I used an iterator when the length of the list was equal to the number entered by the user
-    # and then I used an iterator to display the remaining items
-    def iterator(self, cont_for_1_iter):
-        iterable = iter(self.data.values())
+    def iterator(self, step=10):
         contact_list = []
-        while len(contact_list) != len(self.data.values()):
-            rec = next(iterable)
-            contact_list.append(f'Name: {rec.name.value}, Phones: {rec.phones.value}, Birthday: {rec.birthday.value}')
-        for n in range(0, len(contact_list), cont_for_1_iter):
-            yield contact_list[n:n + cont_for_1_iter]
+        for info in self.data.values():
+            contact_list.append(f'Name: {info.name.value}, Phones: {info.phones.value}, Birthday: {info.birthday.value}')
+        for n in range(0, len(contact_list), step):
+            yield contact_list[n:n + step]
 
 
 class Field:
@@ -43,7 +39,7 @@ class Field:
 class Name(Field):
     def __init__(self, value):
         super().__init__(value)
-        self.__value = value
+        self.value = value
 
     @property
     def value(self):
@@ -55,9 +51,10 @@ class Name(Field):
 
 
 class Phone(Field):
+
     def __init__(self, value):
         super().__init__(value)
-        self.__value = value
+        self.value = value
 
     @property
     def value(self):
@@ -76,7 +73,7 @@ class Phone(Field):
                 for item in new_value:
                     new_list.append(item) if len(item) == 12 and item.isdigit() else print(f'Invalid number: {item}!!!')
                 self.__value = new_list if len(new_list) != 0 else []
-            except:
+            except TypeError:
                 print('Invalid phone format or length, phone must be only digit and 12 numbers!!!')
                 self.__value = []
         elif isinstance(new_value, str):
@@ -90,7 +87,7 @@ class Phone(Field):
 class Birthday(Field):
     def __init__(self, value):
         super().__init__(value)
-        self.__value = value
+        self.value = value
 
     @property
     def value(self):
@@ -98,45 +95,39 @@ class Birthday(Field):
 
     # We are looking for numbers in the string using the regular, if we get a list with 3 values,
     # we make a datetime object, we get the user's age, and if everything passes the validation,
-    # then we assign the value to our datetime object. assign value to None
+    # then we assign the value to our datetime object.
     @value.setter
     def value(self, new_value):
         try:
-            date_numbers = re.findall(r'\d{1,4}', new_value)
-            date_format = datetime(int(date_numbers[2]), int(date_numbers[1]), int(date_numbers[0]))
-            age = datetime.today().year - date_format.year
-            nex_object = datetime(date_format.year, date_format.month, date_format.day).date()
-            value_invalid = len(date_numbers) != 3 or age > 100
-            self.__value = None if value_invalid else nex_object
-            invalid_message = 'Invalid date for class Birthday!!!'
-            valid_message = 'Successfully set date'
-            print(invalid_message if value_invalid else valid_message)
-        except:
-            print("Invalid date for class Birthday!!!")
+            get_date_numbers = re.findall(r'\d{1,4}', new_value)
+            date_format = datetime(int(get_date_numbers[2]), int(get_date_numbers[1]), int(get_date_numbers[0]))
+            get_age = datetime.today().year - date_format.year
+            if len(get_date_numbers) != 3 or get_age > 100:
+                print('Invalid date for class Birthday!!!')
+                self.__value = None
+            else:
+                self.__value = date_format.date()
+                print(f'Successfully set date')
+        except IndexError as info:
+            print(f"Invalid date for class Birthday!!! {info}")
+            self.__value = None
+        except TypeError as info:
+            print(f"Invalid date for class Birthday!!! {info}")
+            self.__value = None
+        except ValueError as info:
+            print(f"Invalid date for class Birthday!!! {info}")
             self.__value = None
 
 
 class Record:
-    # Here, in init, I made sure to immediately assign value to the values that enter the class and there was a check on
-    # what the user entered so that the user, if he entered incorrect values, would use the setter of certain classes
-    def __init__(self, name, phones=Phone(None), birthday=Birthday(None)):
+    def __init__(self, name, phones=Phone(None), birthday=Birthday('1242141214421521')):
         self.name = name
         self.phones = phones
         self.birthday = birthday
-        self.birthday.value = birthday.value
-        self.phones.value = phones.value
 
-    # We do a check of the phone entered by the user, if everything is fine, then we check whether
-    # we had any data in the phone book before, if there was, then we simply append the phone that entered
-    # the function to the list, if we had None before this value, then we assign value to our phone.
-    # if the phone has not passed the validation, we display an error to the user
     def add_phone(self, phones):
         len(phones) == 12 and phones.isdigit() and self.phones.value.append(phones)
         return f'valid number: {phones}!!!' if len(phones) == 12 and phones.isdigit() else f'Invalid number: {phones}!!!'
-    # We check if we have a phone in the data that needs to be changed if there is,
-    # then we check the new phone for correctness If all validations were successful,
-    # then we delete the old phone and add a new one, if something is wrong, we return the error to the user
-    # if the old number is not in the AddressBook data, then we return user error
 
     def change_phone(self, old_phone, new_phone):
         if old_phone in self.phones.value:
@@ -149,54 +140,22 @@ class Record:
         else:
             return 'Error i dont have such phone in my AddressBook!!'
 
-    # Delete the phone that the user entered if it is in the AddressBook data
-    # def delete_phone(self, phone):
-    #     if phone in self.phones.value:
-    #         self.phones.value.remove(phone)
-    #         return f'Successfully deleted phone for user: {self.name.value}!!'
-    #     else:
-    #         return 'Error i dont have such phone in my AddressBook!!'
     def delete_phone(self, phone):
-        phone_in_contacts = phone in self.phones.value
-        phone_in_contacts and self.phones.value.remove(phone)
+        is_phone_in_contacts = phone in self.phones.value
+        is_phone_in_contacts and self.phones.value.remove(phone)
         valid_message = f'Successfully deleted phone for user: {self.name.value}!!'
         error_message = 'Error i dont have such phone in my AddressBook!!'
-        return valid_message if phone_in_contacts else error_message
+        return valid_message if is_phone_in_contacts else error_message
 
-    # We calculate how many days until the birthday of a contact in AddressBook.
-    # We change the year to the current one in order to get the correct values,
-    # we check whether our user's birthday has already passed or not and return the number of days before the birthday
-    def days_to_birthday(self):
-        valid_year = self.birthday.value.replace(year=datetime.now().year)
-        if valid_year.month < datetime.now().month:
-            valid = valid_year - datetime(datetime.now().year - 1, datetime.now().month, datetime.now().day).date()
-            return f'Birthday in {valid.days} days!!!'
+    def get_days_to_birthday(self):
+        get_valid_year = self.birthday.value.replace(year=datetime.now().year)
+        if get_valid_year.month < datetime.now().month:
+            get_days = get_valid_year - datetime(datetime.now().year - 1, datetime.now().month, datetime.now().day).date()
+            return f'Birthday in {get_days.days} days!!!'
         else:
-            valid = valid_year - datetime(datetime.now().year, datetime.now().month, datetime.now().day).date()
-            return f'Birthday in {valid.days} days!!!'
+            get_days = get_valid_year - datetime(datetime.now().year, datetime.now().month, datetime.now().day).date()
+            return f'Birthday in {get_days.days} days!!!'
 
 
 if __name__ == '__main__':
-    Bill = Record(name=Name('Bill'), phones=Phone('43'), birthday=Birthday('aaa'))  # Invalid number: 43 for class Phone!!!
-    print(Bill.birthday.value)  # None
-    print(Bill.phones.value)  # []
-    print(Bill.add_phone('000000000000'))  # valid number: 000000000000!!!
-    print(Bill.phones.value)  # ['000000000000']
-    print(Bill.delete_phone('000000000000'))  # Successfully deleted phone for user: Bill!!
-    print(Bill.phones.value)  # []
-    Bill.birthday.value = '17-10-1998'  # Successfully set date
-    print(Bill.birthday.value)  # 1998-10-17
-    print(Bill.days_to_birthday())  # Birthday in 332 days!!!
-    address = AddressBook()
-    Petya = Record(name=Name('Petya'), phones=Phone('211111111111'), birthday=Birthday('1-1-1997'))
-    Zhenya = Record(name=Name('Zhenya'), phones=Phone('211122222222'), birthday=Birthday('2-1-1897'))  # Invalid date for class Birthday!!!
-    Dima = Record(name=Name('Dima'), phones=Phone('333333333333'), birthday=Birthday('3-1-1997'))
-    Vasya = Record(name=Name('Vasya'), phones=Phone('444444444444'), birthday=Birthday('4-1-1997'))
-    Anton = Record(name=Name('Anton'), phones=Phone('555555555555'), birthday=Birthday('5-1-1997'))
-    Zheka = Record(name=Name('Zheka'), phones=Phone('555555555555'), birthday=Birthday('5-1-1997'))
-    add_rec = address.add_record([Bill, Petya, Zhenya, Dima, Vasya, Anton])
-    address.add_record(Zheka)
-    print(add_rec)  # {'Bill': <__main__.Record object at 0x7fea58027ac0>, 'Petya': <__main__.Record object at 0x7fea5801c250>, 'Zhenya': <__main__.Record object at 0x7fea5801c4f0>, 'Dima': <__main__.Record object at 0x7fea5801d9a0>, 'Vasya': <__main__.Record object at 0x7fea5801dd00>, 'Anton': <__main__.Record object at 0x7fea58023040>, 'Zheka': <__main__.Record object at 0x7fea580232b0>}
-    address_iterator = address.iterator(4)
-    for item in address_iterator:  #  [['Name: Bill, Phones: [], Birthday: 1998-01-01'], ["Name: Petya, Phones: ['211111111111'], Birthday: 1997-01-01"], ["Name: Zhenya, Phones: ['211122222222'], Birthday: None"], ["Name: Dima, Phones: ['333333333333'], Birthday: 1997-01-03"]]
-        print(item)                #  [["Name: Vasya, Phones: ['444444444444'], Birthday: 1997-01-04"], ["Name: Anton, Phones: ['555555555555'], Birthday: 1997-01-05"], ["Name: Zheka, Phones: ['555555555555'], Birthday: 1997-01-05"]]
+    pass
